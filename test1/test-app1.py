19582ca4a21ab9d8d82c98739099229bfd63bf89
worawat <worawat.sittichokpun@gmail.com>
2021-01-28 15:13:03 +0700

    init-pushcode-testing1.v1

---
+++ test1/test-app1.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, make_response, g
+from elasticsearch import Elasticsearch
+from datetime import datetime
+import os
+import socket
+import random
+import json
+
+print(datetime.now())
+
+es=Elasticsearch([{'host':'elasticsearch1','port':9200}])
+app = Flask(__name__)
+
+os.environ['OPTION1'] = 'GPU'
+os.environ['OPTION2'] = 'CPU'
+os.environ['OPTION3'] = 'RAM'
+
+hostname = socket.gethostname()
+
+@app.route('/', methods=['POST','GET'])
+def test1():
+	option = None
+
+	if request.method == 'POST':
+		es=Elasticsearch([{'host':'elasticsearch1','port':9200}])
+		#redis = get_redis()
+
+		option = request.form['option']
+		#data = json.dumps({'timestamp': datetime.now(), 'option': option, 'hostname': hostname})
+		data = {
+			'time': datetime.now(),
+			'option': option,
+			'hostname': hostname
+		}
+
+		#redis.rpush('votes', data)
+		es.indices.create(index='voting', ignore=400)
+		res=es.index(index='voting',body=data)
+
+	resp = make_response(render_template(
+        'test1.html',
+        OPTION1=os.getenv('OPTION1'),
+        OPTION2=os.getenv('OPTION2'),
+		OPTION3=os.getenv('OPTION3'),
+		hostname=hostname
+    ))
+
+	return resp
+	
+	#print(hostname)
+	#print(os.getenv('OPTION1'))
+	#print(os.getenv('OPTION1'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
---
+++ test2/test-app2.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, make_response, g
+from elasticsearch import Elasticsearch
+import json
+import socket
+
+es=Elasticsearch([{'host':'elasticsearch1','port':9200}])
+app = Flask(__name__)
+
+hostname = socket.gethostname()
+
+@app.route('/')
+def test2():
+    RAM = 0
+    CPU = 0
+    GPU = 0
+
+    if request.method == 'GET':
+        es=Elasticsearch([{'host':'elasticsearch1','port':9200}])
+        res = es.search(index='voting', filter_path=['hits.hits._*'], size=10000)
+
+        for each in res['hits']['hits']:
+            print(each)
+            if each['_source']['option'] == 'RAM':
+                RAM = RAM+1
+            if each['_source']['option'] == 'GPU':
+                GPU = GPU+1
+            if each['_source']['option'] == 'CPU':
+                CPU = CPU+1
+
+        resp = make_response(render_template(
+            'test2.html',
+            RAM=RAM,
+            CPU=CPU,
+            GPU=GPU,
+            hostname=hostname
+        ))
+
+        return resp
+    return 'error!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
